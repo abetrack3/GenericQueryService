76a2a463b9c300ed22df946c0732bde3e02de546
Abrar Shahriar <abetrack3@yahoo.com>
2023-01-08 02:47:58 +0600

    initial commit

---
+++ src/main/java/com/abetrack3/GenericQueryService/GenericQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.abetrack3.GenericQueryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericQueryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abetrack3/GenericQueryService/GenericQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abetrack3.GenericQueryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericQueryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
