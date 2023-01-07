e092bf0fa788a39387c96edf8f1af59b9beff324
Abrar Shahriar <abetrack3@yahoo.com>
2023-01-08 03:12:54 +0600

    initial request mapping

--- src/main/java/com/abetrack3/GenericQueryService/GETRequestHandler.java
+++ src/main/java/com/abetrack3/GenericQueryService/GETRequestHandler.java
@@ -1,4 +1,17 @@
 package com.abetrack3.GenericQueryService;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
 public class GETRequestHandler {
+
+    @GetMapping(value="/")
+    public String handleQuery(@RequestParam Map param) {
+        System.out.println(param);
+        System.out.println("param.size() = " + param.size());
+        return "Request Received";
+    }
+
 }
