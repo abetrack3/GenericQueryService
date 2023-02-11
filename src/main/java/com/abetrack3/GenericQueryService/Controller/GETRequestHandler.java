10d4bdffb5b4d13a2f42e9a3f05258b3c9d849ee
Abrar Shahriar <abetrack3@yahoo.com>
2023-02-11 23:58:21 +0600

    re organized the code a little bit

--- src/main/java/com/abetrack3/GenericQueryService/Controller/GETRequestHandler.java
+++ src/main/java/com/abetrack3/GenericQueryService/Controller/GETRequestHandler.java
@@ -1,26 +1,52 @@
 package com.abetrack3.GenericQueryService.Controller;
 
+import com.abetrack3.GenericQueryService.Controller.Data.DatabaseNameProvider;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class GETRequestHandler {
 
     @GetMapping(value="/")
-    public String onQueryRequestReceived(
+    public ResponseEntity<Object> onQueryRequestReceived(
         @RequestParam MultiValueMap<String, String> multiMap,
         @RequestHeader Map<String, String> requestHeaders
     ) {
 
-        System.out.println("multiMap = " + multiMap);
-        System.out.println("requestHeaders = " + requestHeaders);
+        String serviceId = requestHeaders.get("x-service-id");
+
+        if (serviceId == null) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("x-service-id is mandatory");
+        }
+
+        String databaseName = DatabaseNameProvider.getDatabaseName(serviceId);
+
+        if (databaseName == null) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("provided x-service-id is not registered in the dictionary");
+        }
+
+        List<String> queryIds = multiMap.get("id");
+        List<String> queryValues = multiMap.get("values");
+
+        if (queryValues.size() != queryIds.size()) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Mismatch in number of query ids and queryValues");
+        }
 
 
-        return "multiMap = " + multiMap.toString()
-                + "\n" +
-                "x-service-id = " + requestHeaders.get("x-service-id");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Development in progress");
     }
 
 }
