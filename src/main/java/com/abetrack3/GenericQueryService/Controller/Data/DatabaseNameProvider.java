9787b1399ca5a2ca53d6f1a8ba458650f16cecd9
Abrar Shahriar <abetrack3@yahoo.com>
2023-02-11 23:57:52 +0600

    implemented databasename provider

---
+++ src/main/java/com/abetrack3/GenericQueryService/Controller/Data/DatabaseNameProvider.java
@@ -0,0 +1,21 @@
+package com.abetrack3.GenericQueryService.Controller.Data;
+
+import java.util.Map;
+
+import static java.util.Map.entry;
+
+public class DatabaseNameProvider {
+
+    private static final Map<String, String> xServiceIdToDbNameMap;
+
+    static {
+        xServiceIdToDbNameMap = Map.ofEntries(
+                entry("product", "ProductReadDatabase")
+        );
+    }
+
+    public static String getDatabaseName(String xServiceId) {
+        return xServiceIdToDbNameMap.get(xServiceId);
+    }
+
+}
